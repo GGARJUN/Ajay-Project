fb1209115d60e80d4fb03b8ad107a8ae784a63b1
GGARJUN <techarjung@gmail.com>
2024-02-27 16:12:17 +0530

    some changes

--- src/app/dashboard/DashboardNav.tsx
+++ src/app/dashboard/DashboardNav.tsx
@@ -6,15 +6,13 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import * as React from "react"
- 
+import * as React from "react";
 
 import Link from "next/link";
 
 export function DashboardNav() {
   return (
     <nav className=" z-[99999]   fixed  top-0 mt-2 left-[20%] hidden  md:flex items-center  w-[50%] justify-evenly">
-
       <Link href="/dashboard">
         <h1 className=" text-base font-semibold py-2 px-7 rounded-md hover:bg-green-600">
           Home
@@ -25,33 +23,35 @@ export function DashboardNav() {
           About
         </h1>
       </Link>
-      <Link href="/blogs">
+      <Link href="/team">
         <h1 className="text-base font-semibold py-2 px-7 rounded-md hover:bg-green-600">
-          Blog
+          Our Team
         </h1>
       </Link>
       <Link href="/contect">
         <h1 className="text-base font-semibold py-2 px-7 rounded-md hover:bg-green-600">
-         Support
+          Support
         </h1>
       </Link>
       {/* <Link href="/pages">Pages</Link> */}
 
       <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button  className=" text-md w-20">Pages</Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="  w-36">
-            <DropdownMenuGroup>
-              <DropdownMenuItem><Link href="/pages">Blog</Link></DropdownMenuItem>
-              <DropdownMenuItem>Blog Details</DropdownMenuItem>
-              <DropdownMenuItem>Settings</DropdownMenuItem>
-              <DropdownMenuItem>Keyboard </DropdownMenuItem>
-            </DropdownMenuGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
-
-      
+        <DropdownMenuTrigger asChild>
+          <h1 className="text-base font-semibold py-2 px-7 rounded-md hover:bg-green-600">
+            Our Futures
+          </h1>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className=" cursor-pointer w-36">
+          <DropdownMenuGroup>
+            <Link href="/blogs">
+              <DropdownMenuItem>Blog</DropdownMenuItem>
+            </Link>
+            <DropdownMenuItem>Blog Details</DropdownMenuItem>
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Keyboard </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </nav>
   );
 }
---
+++ src/app/team/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { DashboardNav } from '../dashboard/DashboardNav'
+
+export default function page  ()  {
+  return (
+    <>
+        <DashboardNav/>
+        <div className='mt-20'>
+            <h1> The Team </h1>
+        </div>
+    </>
+  )
+}
